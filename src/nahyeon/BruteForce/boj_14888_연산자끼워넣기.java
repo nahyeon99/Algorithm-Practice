bc858c88dd73d56e684dd4a5995f2492fcc7d16f
nahyeon99 <nahyeonee99@gmail.com>
2023-07-01 17:22:58 +0900

    [BruteForce] baekjoon-14888

---
+++ src/nahyeon/BruteForce/boj_14888_연산자끼워넣기.java
@@ -0,0 +1,104 @@
+package nahyeon.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_14888_연산자끼워넣기 {
+
+    static FastReader scan = new FastReader();
+
+    static int N, max, min;
+    static int[] nums, operators, order;
+
+    static void input() {
+        N = scan.nextInt();
+        nums = new int[N+1];
+        operators = new int[5];
+        order = new int[N];
+
+        for (int i = 1; i <= N; i++)
+            nums[i] = scan.nextInt();
+
+        for (int i = 1; i <= 4; i++)
+            operators[i] = scan.nextInt();
+
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+    }
+
+    static int calculate() {
+        int value = nums[1];
+        for (int i = 1; i <= N-1; i++) {
+            if (order[i] == 1)
+                value = value + nums[i+1];
+            if (order[i] == 2)
+                value = value - nums[i+1];
+            if (order[i] == 3)
+                value = value * nums[i+1];
+            if (order[i] == 4)
+                value = value / nums[i+1];
+        }
+        return value;
+    }
+    static void rec_func(int k) {
+        if (k == N) {
+            int value = calculate();
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+        } else {
+            for (int cand = 1; cand <= 4; cand++) {
+                if (operators[cand] >= 1) {
+                    operators[cand]--;
+                    order[k] = cand;
+                    rec_func(k+1);
+                    operators[cand]++;
+                    order[k] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1);
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(br.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
