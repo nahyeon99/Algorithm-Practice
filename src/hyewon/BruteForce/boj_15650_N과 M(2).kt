3134e0c7661d95b9c5bd121699c4f3f0d7a529ef
altpfwlzh <altpfwlzh@gmail.com>
2023-08-10 17:56:02 +0900

    [BruteForce] baekjoon-15650

--- src/hyewon/BruteForce/boj_15650_N과 M(2).kt
+++ src/hyewon/BruteForce/boj_15650_N과 M(2).kt
@@ -1,33 +1,57 @@
 package hyewon.BruteForce
 
-lateinit var check: BooleanArray
-lateinit var ans: IntArray
-val sb = StringBuilder()
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
-fun main() = with(System.`in`.bufferedReader()) {
+/**현재 버전 : 47번째 고르기 조건이 세련되지 않다. 
+ * #분류 ) BruteForce
+ * #포인트 ) N개중 중복이 불가능 하고 * 고르기
+ * #정답의 최대치 ) N : 8, M : 8, ans : 최대 8자리 수인 배열임(IntArray)
+ * #시간 복잡도 ) O(N!/M!(N-M)!)
+ * #공간 복잡도 ) O(m)
+ * #메모
+ *
+ *
+ */
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+private lateinit var used: IntArray
+private lateinit var ans: IntArray
 
 
+fun main() {
 
-    val (n, m) = readLine().split(" ").map { it.toInt() }
-    check = BooleanArray(n + 1)
-    ans = IntArray(m)
-    go(0, 1, n, m)
-    println(sb)
+    //@Input : n, m
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    ans = IntArray(m+1){1} //정답 초기값 : 모든 자리수가 1인 수
+    used = IntArray(n+1){0} //중복 체크용 배열 : 사용한 것이면 1 아니면 0
+
+    recFun(n, m, 1) //n : 수의 범위, m : 자리수, k : 현재 증가하고 있는 자리수
+
+    bw.flush()
+    bw.close()
 }
 
-private fun go(idx: Int, start : Int, n: Int, m: Int) {
-    if (idx == m) {
-        ans.forEach { sb.append(it).append(' ') }
-        sb.append("\n")
-        return
-    }
+private fun recFun(n : Int, m : Int, k : Int){
+    //종료 조건
+    if(k == m+1 ){
+        for(i in 1 .. m) bw.write("${ans[i]} ")
+        bw.write("\n")
+    } else {
+        for(num in 1..n){
+            //중복 불가능 : 사용한 수인지 체크
+            //고르기(오름차순) : 이전의 수보다 작은지 체크
+            if(used[num]==1 || ans[k-1] > num) continue
 
-    for (i in start..n) {
-        if (check[i]) continue
-        check[i] = true
-        ans[idx] = i
-        go(idx + 1, i + 1, n, m)
-        check[i] = false
+            ans[k] = num
+            used[num] = 1 //중복 불가능 : 사용한 수면 1로 바꾸기
+
+            recFun(n, m, k+1)
+
+            used[num] = 0 //중복 불가능 : 사용한 수 끝나면 0으로 바꾸기
+        }
     }
 }
-
