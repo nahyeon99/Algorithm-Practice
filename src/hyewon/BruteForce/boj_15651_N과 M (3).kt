3f48b3becf7da1b05a1cb431d056ee9a71e2af6e
altpfwlzh <altpfwlzh@gmail.com>
2023-08-10 17:59:40 +0900

    [BruteForce] baekjoon-15651

--- src/hyewon/BruteForce/boj_15651_N과 M (3).kt
+++ src/hyewon/BruteForce/boj_15651_N과 M (3).kt
@@ -1,36 +1,48 @@
 package hyewon.BruteForce
 
+import java.io.*
 
+/**
+ * #분류 ) BruteForce
+ * #포인트 ) N개중 중복이 가능 하고 * 순서 있게 나열 하기
+ * #정답의 최대치 ) N : 7, M : 7, ans : 최대 7자리 수인 배열임(IntArray)
+ * #시간 복잡도 ) O(n^m)
+ * #공간 복잡도 ) O(m)
+ * #메모
+ * 1. 초기 : 모든 자리의 수가 1로 시작
+ * 2. 0번째 자리수부터 1~n까지 돌리는데
+ * 3. 재귀 함수 넣어서 다음 자리도 1~n까지 돌려 주기.
+ * 4. 끝자리 수에 값넣을 때까지 출력 안 함함 */
+
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+private lateinit var ans: IntArray
 
 fun main() {
-    lateinit var arr: Array<Int>
 
-    var n = 0
-    var m = 0
-    val sb = StringBuilder()
+    //@Input : n, m
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    ans = IntArray(m){1} //정답 초기값 : 모든 자리수가 1인 수
 
-    val nm = readLine()!!.split(" ")
-    m = nm[1].toInt()
-    n = nm[0].toInt()
+    recFun(n, m, 0) //n : 수의 범위, m : 자리수, k : 현재 증가하고 있는 자리수
 
-    arr = Array(n + 1) { i -> i }
-
-    dfs(1, 0, "")
-    print(sb.toString())
-
-
-
+    //@Output : ans
+    bw.flush()
+    bw.close()
 }
-private fun dfs(idx: Int, len: Int, str: String) {
-    if (len == hyewon.BruteForce.m) {
-        sb.append(str).append("\n")
-        return
+
+private fun recFun(n: Int, m: Int, k: Int){
+    if(k==m){ //끝자리 수까지 수를 정해주어서 index넘어 섰으면
+        for(i in 0 until m){ // ans write
+            bw.write("${ans[i]} ")
+        }
+        bw.write("\n")
+    } else {
+        for(num in 1..n){ //첫번째 자리 수(index 0)부터 차근차근 ++ 해주기
+            ans[k] = num
+            recFun(n, m, k+1) //다음 자리수 수 같은 방법으로 1~n돌리면서 값을 넣어주기 위해서 k+1 해서 재귀 함수
+        }
     }
 
-    for (i in 1..hyewon.BruteForce.n) {
-        if (len == 0)
-            dfs(i, 1, hyewon.BruteForce.arr[i].toString())
-        else
-            dfs(i, len + 1, "$str ${hyewon.BruteForce.arr[i]}")
-    }
 }
